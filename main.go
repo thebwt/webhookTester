da2f194b684e04347f80144d8dc3e3763f53c812
Brandon Tomlinson <thebwt@gmail.com>
2017-09-18 12:56:41 -0500

    outputing request information

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Fprintf(w, "This is working! %s\n", r.RequestURI)
+	fmt.Fprintf(w, "Second line! %s\n", body)
+	fmt.Fprintf(w, "Third line! %s", r.Header)
+	//Grab variables: headers, multipart form payload, URI
+}
 
 func main() {
-	fmt.Println("Testing!")
+	fmt.Println("testing")
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/{.*}", HomeHandler)
+
+	http.ListenAndServe("0.0.0.0:8080", r)
 }
